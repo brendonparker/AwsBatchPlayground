85de2901c22471ad547be6f39c9f8b694162be99
Brendon Parker <bparker@icsfl.com>
2021-05-18 12:39:07 -0400

    Initial commit

---
+++ src/AwsBatchPlayground/AwsBatchPlaygroundStack.cs
@@ -0,0 +1,12 @@
+using Amazon.CDK;
+
+namespace AwsBatchPlayground
+{
+    public class AwsBatchPlaygroundStack : Stack
+    {
+        internal AwsBatchPlaygroundStack(Construct scope, string id, IStackProps props = null) : base(scope, id, props)
+        {
+            // The code that defines your stack goes here
+        }
+    }
+}
