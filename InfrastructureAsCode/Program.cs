712087233f4ec94d97da9e47b84540f98461eef8
Brendon Parker <bparker@icsfl.com>
2021-05-18 12:42:19 -0400

    Initial commit

---
+++ InfrastructureAsCode/Program.cs
@@ -0,0 +1,18 @@
+﻿using Amazon.CDK;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace AwsBatchPlayground
+{
+    sealed class Program
+    {
+        public static void Main(string[] args)
+        {
+            var app = new App();
+            new AwsBatchPlaygroundStack(app, "AwsBatchPlaygroundStack", new StackProps());
+
+            app.Synth();
+        }
+    }
+}
