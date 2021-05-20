e1f07b741bacec96ee7c12ffd352b7281926b4df
Brendon Parker <bparker@icsfl.com>
2021-05-20 08:16:39 -0400

    Use StepFunction tasks for dynamodb instead of custom lambdas.

--- InfrastructureAsCode/AwsStepFunctionStack.cs
+++ InfrastructureAsCode/AwsStepFunctionStack.cs
@@ -61,43 +61,7 @@ namespace InfrastructureAsCode
                     }
             });
 
-            var lambdaDuplicateCheck = new Lambda.Function(this, "LambdaDuplicateCheck", new Lambda.FunctionProps
-            {
-                FunctionName = "Step-DuplicateCheck",
-                Runtime = Lambda.Runtime.DOTNET_CORE_3_1,
-                MemorySize = 1024,
-                Code = Lambda.Code.FromAsset("./LambdaJobCheck/bin/Release/netcoreapp3.1/linux-x64"),
-                Handler = "LambdaJobCheck::LambdaJobCheck.Function::CheckForDuplicateJobAsync",
-                Timeout = Duration.Seconds(30),
-                Environment = new Dictionary<string, string>
-                {
-                    { "TABLE_NAME", TABLE_NAME }
-                },
-                Role = lambdaRole
-            });
-
-            var lambdaCleanup = new Lambda.Function(this, "LambdaCleanup", new Lambda.FunctionProps
-            {
-                FunctionName = "Step-Cleanup",
-                Runtime = Lambda.Runtime.DOTNET_CORE_3_1,
-                MemorySize = 1024,
-                Code = Lambda.Code.FromAsset("./LambdaJobCheck/bin/Release/netcoreapp3.1/linux-x64"),
-                Handler = "LambdaJobCheck::LambdaJobCheck.Function::CleanupJobAsync",
-                Timeout = Duration.Seconds(30),
-                Environment = new Dictionary<string, string>
-                {
-                    { "TABLE_NAME", TABLE_NAME }
-                },
-                Role = lambdaRole
-            });
-
-            //var duplicateCheck = new SF.Tasks.LambdaInvoke(this, "DuplicateCheck", new SF.Tasks.LambdaInvokeProps
-            //{
-            //    LambdaFunction = lambdaDuplicateCheck,
-            //    OutputPath = "$.Payload"
-            //});
-
-            var duplicateCheck = new SF.Tasks.DynamoPutItem(this, "DynamoDuplicateCheck", new SF.Tasks.DynamoPutItemProps
+            var duplicateCheck = new SF.Tasks.DynamoPutItem(this, "DuplicateCheck", new SF.Tasks.DynamoPutItemProps
             {
                 Table = table,
                 Item = new Dictionary<string, SF.Tasks.DynamoAttributeValue>
@@ -105,12 +69,13 @@ namespace InfrastructureAsCode
                     { "JobKey", SF.Tasks.DynamoAttributeValue.FromString(SF.JsonPath.StringAt("$.JobKey")) },
                     { "CustomerId", SF.Tasks.DynamoAttributeValue.FromString(SF.JsonPath.StringAt("$.CustomerId")) }
                 },
-                OutputPath = "$.Payload",
                 ConditionExpression = "attribute_not_exists(#k)",
                 ExpressionAttributeNames = new Dictionary<string, string>
                 {
                     { "#k", "JobKey" }
-                }
+                },
+                // Keep original Input, tack on output into $.DuplicateCheck
+                ResultPath = "$.DuplicateCheck"
             });
 
             var startJob = new SF.Tasks.BatchSubmitJob(this, "SubmitJob", new SF.Tasks.BatchSubmitJobProps
@@ -128,17 +93,24 @@ namespace InfrastructureAsCode
                 })
             });
 
-            var cleanup = new SF.Tasks.LambdaInvoke(this, "Cleanup", new SF.Tasks.LambdaInvokeProps
+            var cleanup = new SF.Tasks.DynamoDeleteItem(this, "Cleanup", new SF.Tasks.DynamoDeleteItemProps
             {
-                LambdaFunction = lambdaCleanup
+                Table = table,
+                Key = new Dictionary<string, SF.Tasks.DynamoAttributeValue>
+                {
+                    { "JobKey", SF.Tasks.DynamoAttributeValue.FromString(SF.JsonPath.StringAt("$.JobKey")) }
+                }
             });
 
             var stateAlreadyRunning = new SF.Succeed(this, "JobAlreadyRunning");
 
             var definition = duplicateCheck
-                .Next(new SF.Choice(this, "JobRunningChoice")
-                    .When(SF.Condition.BooleanEquals("$.JobAlreadyRunning", true), stateAlreadyRunning)
-                    .When(SF.Condition.BooleanEquals("$.JobAlreadyRunning", false), startJob.Next(cleanup)));
+                .AddCatch(stateAlreadyRunning, new SF.CatchProps
+                {
+                    Errors = new[] { "DynamoDB.ConditionalCheckFailedException" }
+                })
+                .Next(startJob)
+                .Next(cleanup);
 
             new SF.StateMachine(this, "StateMachine", new SF.StateMachineProps
             {
